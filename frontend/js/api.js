ade9c4a8af13deea894a8aa8053cd110fe57196c
Ariel Armijo <arielarmijo@yahoo.es>
2021-01-16 16:54:21 -0300

    Corrige url api

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,1 +1,2 @@
-export const apiUrl = "http://localhost:8080/monkeyweather/";
+//export const apiUrl = "http://localhost:8080/monkeyweather/";
+export const apiUrl = "http://monkeycode.tk/monkeyweather-userapi/";
