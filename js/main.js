18d4adcd1a9e792346f853111b9607201d2033a5
edgar <harove@gmail.com>
2021-01-15 22:47:35 -0300

    rendering weather icon

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
+const apiKey = 'd47bc5e52c9b46f24443d3c321a13ddc';
+const city = 'London';
 
-const sayHi = () =>{
-  alert('Hi')
+async function getWeather(){
+  const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`);
+  debugger
+  const response = await api_call.json();
+
+  document.getElementById('weatherImage').src=`http://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`
+  console.log(response);
 }
+
