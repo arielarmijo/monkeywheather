936fcc493c184834bd8daf21bd0e2af907a170c8
edgar <harove@gmail.com>
2021-01-16 14:48:10 -0300

    getting temperatures on chart

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
 const apiKey = '72216e8ac7dc0f927f7aa3d9b2af7c3f';
 
-
+fahrenheitInCelsius = k => Math.floor(k-273.15);
 
 async function getWeather(city){
   const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`);
   const response = await api_call.json();
-  debugger
   const api_call_forecast = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`)
   const responseForecast = await api_call_forecast.json();
 
@@ -14,7 +13,20 @@ async function getWeather(city){
   document.getElementById('temp_max').innerHTML=response.main.temp_max;
   document.getElementById('temp_min').innerHTML=response.main.temp_min;
   console.log(response);
-
+  console.log(responseForecast);
+  var trace = { 
+    x: responseForecast.list.filter((record,index)=>{
+      if (index%8===0 )
+        return record
+    }).map(record=>record.dt_txt),        
+    y: responseForecast.list.filter((record,index)=>{
+      if (index%8===0 )
+        return record
+    }).map(record=>fahrenheitInCelsius(record.main.temp)),      
+    mode: 'line'    
+  };
+  console.log(trace)
+  Plotly.newPlot('myDiv', [trace]);
 }
 
 
