77bc5120a5b59e84bc9c9a747cd1173f75fcd573
edgar <harove@gmail.com>
2021-01-16 00:36:26 -0300

    icon on click

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
-const apiKey = 'd47bc5e52c9b46f24443d3c321a13ddc';
-const city = 'London';
+const apiKey = '72216e8ac7dc0f927f7aa3d9b2af7c3f';
 
-async function getWeather(){
+
+
+
+
+
+async function getWeather(city){
   const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`);
-  debugger
   const response = await api_call.json();
-
   document.getElementById('weatherImage').src=`http://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`
   console.log(response);
 }
+
+
+function getWeatherButton(){
+  const city = document.getElementById('cityInput').value;
+  getWeather(city);
+}
+
+
 
