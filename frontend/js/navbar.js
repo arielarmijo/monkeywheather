2851d7582ed77e89f2bb8f3425d216452f6f8683
Ariel Armijo <arielarmijo@yahoo.es>
2021-01-17 08:36:39 -0300

    Integra home con login

--- frontend/js/navbar.js
+++ frontend/js/navbar.js
@@ -1,7 +1,9 @@
 document.addEventListener('DOMContentLoaded', async function () {
 
     let response = await fetch("snippets/navbar.html");
-    let navbar = await response.text();
-    document.querySelector("nav").innerHTML = navbar;
+    let navbarTemplate = await response.text();
+    let navbar = document.querySelector("nav");
+    navbar.classList.add("navbar", "navbar-light", "bg-light");
+    navbar.innerHTML = navbarTemplate;
     
 });
