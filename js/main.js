5b3c23ddce3ea1feeb46a174b63346897bc8ec69
edgar <harove@gmail.com>
2021-01-17 02:03:34 -0300

    forcast with icons

--- js/main.js
+++ js/main.js
@@ -8,15 +8,16 @@ async function getWeather(city){
   const response = await api_call.json();
   const api_call_forecast = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`)
   const responseForecast = await api_call_forecast.json();
-
-
+  const iconsArray = plotForecastIcons(responseForecast)
+  renderForecast(iconsArray)
   document.getElementById('time').innerHTML= moment.unix(response.dt).format('DD-MM-YYYY');
-
   document.getElementById('weatherImage').src=`http://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`
   document.getElementById('description').innerHTML=response.weather[0].description;
   document.getElementById('temp_max').innerHTML=response.main.temp_max;
   document.getElementById('temp_min').innerHTML=response.main.temp_min;
   document.getElementById('city').innerHTML=response.name;
+
+
   console.log(response);
   console.log(responseForecast);
   var trace = { 
@@ -40,15 +41,9 @@ async function getWeather(city){
     }, 
   };
 
-  var config={
-    displayModeBar: false, // this is the line that hides the plotly bar.
-  }
-
-  console.log(trace)
-  Plotly.newPlot(
-    'myDiv', 
-    [trace],
-    {
+  var layout = {
+    width: 380,
+    height: 250,
     title: 'Forecast del tiempo para los próximos dias',
     showlegend: true,
     margin: 
@@ -59,19 +54,39 @@ async function getWeather(city){
         t: 40,
       },
     hovermode: false,
-    },
+    }
+
+  var config={
+    displayModeBar: false, // this is the line that hides the plotly bar.
+  }
+
+  Plotly.newPlot(
+    'myDiv', 
+    [trace],
+    layout,
     config
   );
-
-
 
 }
 
 
 function getWeatherButton(){
   const city = document.getElementById('cityInput').value;
-  getWeather(city);
+  city !=='' ? getWeather(city): getWeather('Santiago') ;
 }
 
+function plotForecastIcons(forecastObj){
+  iconsArray = forecastObj.list.filter((record,index)=>index%8===0).map(record=>record.weather[0].icon)
+  return iconsArray
+}
+
+function renderForecast(forecastArray){
+  for (i=0;i<5;i++){
+    document.getElementById('fore'+i.toString()).src=`http://openweathermap.org/img/wn/${forecastArray[i]}@2x.png`
+    debugger
+  }
+ 
+
+}
 
 
