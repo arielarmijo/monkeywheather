c172c2a4e7625db830d88e8e760e79295ec2b073
edgar <harove@gmail.com>
2021-01-17 15:53:12 -0300

    weather cards forecast

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -4,9 +4,9 @@ const date = moment()
 fahrenheitInCelsius = k => Math.floor(k-273.15);
 
 async function getWeather(city){
-  const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`);
+  const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&lang=sp&appid=${apiKey}`);
   const response = await api_call.json();
-  const api_call_forecast = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`)
+  const api_call_forecast = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&lang=sp&appid=${apiKey}`)
   const responseForecast = await api_call_forecast.json();
   const iconsArray = plotForecastIcons(responseForecast)
 
@@ -14,14 +14,14 @@ async function getWeather(city){
   document.getElementById('time').innerHTML= moment.unix(response.dt).format("dddd, MMMM Do YYYY, h:mm:ss a")
   document.getElementById('weatherImage').src=`http://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`
   document.getElementById('description').innerHTML=response.weather[0].description;
-  document.getElementById('temp_max').innerHTML=response.main.temp_max;
-  document.getElementById('temp_min').innerHTML=response.main.temp_min;
+  document.getElementById('temp_max').innerHTML='Temperatura máxima: ' +  response.main.temp_max + 'ºC';
+  document.getElementById('temp_min').innerHTML='Temperatura mínima: ' +  response.main.temp_min + 'ºC' ;
   document.getElementById('city').innerHTML=response.name;
 
 
   console.log(response);
   console.log(responseForecast);
-  var trace = { 
+  var tempMax = { 
     x: responseForecast.list.filter((record,index)=>{
       if (index%8===0 )
         return record
@@ -29,18 +29,40 @@ async function getWeather(city){
     y: responseForecast.list.filter((record,index)=>{
       if (index%8===0 )
         return record
-    }).map(record=>fahrenheitInCelsius(record.main.temp)),      
+    }).map(record=>record.main.temp_max),      
     text: responseForecast.list.filter((record,index)=>{
       if (index%8===0 )
         return record
-    }).map(record=>fahrenheitInCelsius(record.main.temp)),
+    }).map(record=>record.main.temp_max),
     textposition: 'auto',
     cliponaxis: false,
     type: 'line',   
     marker: {
       color: '#5b9aa0'
     }, 
-    name:'Temperatura'
+    name:'Temperatura máxima'
+  };
+
+  var tempMin = { 
+    x: responseForecast.list.filter((record,index)=>{
+      if (index%8===0 )
+        return record
+    }).map(record=>record.dt_txt.substring(0,10)),        
+    y: responseForecast.list.filter((record,index)=>{
+      if (index%8===0 )
+        return record
+    }).map(record=>record.main.temp_min),      
+    text: responseForecast.list.filter((record,index)=>{
+      if (index%8===0 )
+        return record
+    }).map(record=>record.main.temp_min),
+    textposition: 'auto',
+    cliponaxis: false,
+    type: 'line',   
+    marker: {
+      color: '#622569'
+    }, 
+    name:'Temperatura mínima'
   };
 
   var layout = {
@@ -64,7 +86,7 @@ async function getWeather(city){
 
   Plotly.newPlot(
     'myDiv', 
-    [trace],
+    [tempMax,tempMin],
     layout,
     config
   );
@@ -81,7 +103,10 @@ function plotForecastIcons(forecastObj){
   iconsArray = forecastObj.list.filter((record,index)=>index%8===0).map(record=>{
     return({
       icon:record.weather[0].icon,
-      dt: moment.unix(record.dt).format("dddd, DD")
+      dt: moment.unix(record.dt).format("dddd, DD"),
+      description:record.weather[0].description,
+      tempMax: record.main.temp_max,
+      tempMin: record.main.temp_min
     })
     
   })
@@ -92,6 +117,9 @@ function renderForecast(forecastArray){
   for (i=0;i<5;i++){
     document.getElementById('fore'+i.toString()).src=`http://openweathermap.org/img/wn/${forecastArray[i].icon}@2x.png`
     document.getElementById('day'+i.toString()).innerHTML=forecastArray[i].dt
+    document.getElementById('desc'+i.toString()).innerHTML=forecastArray[i].description
+    document.getElementById('tempMax'+i.toString()).innerHTML='max: '+forecastArray[i].tempMax + 'ºC'
+    document.getElementById('tempMin'+i.toString()).innerHTML='min: '+forecastArray[i].tempMin + 'ºC'
   }
  
 
