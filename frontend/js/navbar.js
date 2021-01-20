939952ca9fd93b72e4f96e63dae462d7c8f12f3a
Ariel Armijo <arielarmijo@yahoo.es>
2021-01-20 10:10:23 -0300

    Organiza rama angular-frontend

--- frontend/js/navbar.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', async function () {
-
-    let response = await fetch("snippets/navbar.html");
-    let navbarTemplate = await response.text();
-    let navbar = document.querySelector("nav");
-    navbar.classList.add("navbar", "navbar-light", "bg-light");
-    navbar.innerHTML = navbarTemplate;
-    
-});
